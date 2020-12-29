d1eef24c7e9b9dc1e931600205b45456f107483a
Mudasir <mudasiralinizamani@gmail.com>
2020-12-29 23:06:58 +0500

    IDK

--- fourth/settings.py
+++ fourth/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#vsahgk^m^lj_*a9oq%$yoq9hf)f5du-z!@zpn$2==aub7gl4b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-react-myweb-1.herokuapp.com', '127.0.0.1']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
